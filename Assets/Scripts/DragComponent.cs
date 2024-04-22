1fbfd7319a9d0df2c39ff7a50654c988c5c82d0d
Evaline <evaline@relugames.com>
2024-04-22 17:51:36 +0900

    sticky notes drag

---
+++ Assets/Scripts/DragComponent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragComponent : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("being drag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("end drag");
+    }
+
+}
