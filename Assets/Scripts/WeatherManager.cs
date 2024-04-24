330cc8f98b08c5b6fc0e70a3703625e071d0eaf9
Evaline <evaline@relugames.com>
2024-04-24 13:30:24 +0900

    use coroutine to fetch weather info

--- Assets/Scripts/WeatherManager.cs
+++ Assets/Scripts/WeatherManager.cs
@@ -8,6 +8,7 @@ public class WeatherManager : MonoBehaviour
 
     [SerializeField] private WeatherAPIManager weatherAPIManager;
     [SerializeField] private TMPro.TextMeshProUGUI weatherDescriptionText;
+    [SerializeField] private float timeoutSeconds = 3f;
 
     private bool isOpen;
     private void Awake()
@@ -16,21 +17,34 @@ public class WeatherManager : MonoBehaviour
     }
     private void OnEnable()
     {
-        UpdateWeatherDescription();
-        UpdateWeatherConditionCode();
+        StartCoroutine(UpdateWeatherDescriptionWithTimeout());
 
     }
-    private void UpdateWeatherDescription()
+    private IEnumerator UpdateWeatherDescriptionWithTimeout()
     {
-        string weatherDescription = weatherAPIManager.GetWeatherDescription();
+        bool completed = false;
+        StartCoroutine(UpdateWeatherInformation(() => completed = true));
+
+        float startTime = Time.time;
+        while (!completed && Time.time - startTime < timeoutSeconds)
+        {
+            yield return null;
+        }
+        if (!completed)
+        {
+            weatherDescriptionText.text = "Failed to get weather data";
+            Debug.Log("Failed to fetch weather description in time, using default.");
+        }
+    
+    }
+    private IEnumerator UpdateWeatherInformation(System.Action onComplete)
+    {
+        WeatherData weatherData = WeatherAPIHelper.GetWeatherData();
+        string weatherDescription = weatherData.weather[0].description;
         weatherDescriptionText.text = $"Current Weather: {weatherDescription}";
+        onComplete?.Invoke();
+        yield return null;
     }
-    private void UpdateWeatherConditionCode()
-    {
-        float weatherConditionCode = weatherAPIManager.GetWeatherConditionCode();
-        Debug.Log(weatherConditionCode);
-    }
-
     public void SetOpenStatus(bool isOpen)
     {
         this.isOpen = isOpen;
